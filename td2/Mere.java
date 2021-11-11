f968dfd29f695ad3342e4ab5f93a2163b5e77eb8
Ytek24 <60788638+Ytek24@users.noreply.github.com>
2021-11-11 11:59:40 +0100

    Add files via upload

---
+++ td2/Mere.java
@@ -0,0 +1,30 @@
+package td2;
+class Mere {
+	protected int meth=42;
+	public void printMeth() {
+		System.out.println(meth);
+		}
+	}
+class Fille extends Mere {
+	public int meth = 24;
+}
+class Main {
+	public static void main(String[] args) {
+		Mere mere = new Mere();
+		System.out.println(mere.meth);
+
+		mere.printMeth();
+	
+		Fille fille = new Fille();
+	
+		System.out.println(fille.meth);
+	
+		fille.printMeth();
+	
+		Mere mereFille = new Fille();
+	
+		System.out.println(mereFille.meth);
+	
+		mereFille.printMeth();
+}
+}
